7399b6eae3e63453a33b9edd8cad9fa52c573f29
Danilo Nogueira <danilo.barion1986@live.com>
2015-11-02 00:05:50 -0200

    Teste Final

--- Scripts/Angular/testes.js
+++ Scripts/Angular/testes.js
@@ -7,7 +7,4 @@ app.controller('TesteCtrl', function($scope, $http) {
 		
 		$http.get("http://danilobarion1986.github.io/Data/JSON/json_teste.json")
 		.success(function (response) {$scope.usuarios = response;});
-
-		$http.get("http://danilobarion1986.github.io/Data/XML/xml_teste.xml")
-		.success(function (response) {$scope.usuarios_xml = response;});
 });
