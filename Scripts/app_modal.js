3b92ed448f396aa236b2c4a3bf67291218db5980
Danilo Barion Nogueira <danilo.barion1986@live.com>
2015-12-04 13:44:36 -0200

    Create app_modal.js

---
+++ Scripts/app_modal.js
@@ -0,0 +1,11 @@
+function fecharModal(nome){
+	document.getElementById(nome).style.display='none';
+}
+
+function abrirModal(nome){
+	document.getElementById(nome).style.display='block';
+}
+
+function salvar(tipo){
+	
+}
